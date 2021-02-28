7cd81bc599ff233dc9ab30f451c3999793526309
Soraya <svalleortega@gmail.com>
2021-02-28 11:03:24 +0100

    Function for splitting and filtering input tags

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ textarea.addEventListener('keyup', (e) => {
 });
 
 function createTags(input) {
-  console.log(input)
-}
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+  console.log(tags)
+};
